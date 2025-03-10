7ae91d0b380d7d80dd8fec7f7a4445a3b44b9cbe
florintudor2k <40282705-florintudor2k@users.noreply.replit.com>
2025-03-10 09:12:50 +0000

    Restored to 'bab961a887e155db6fad5e75f5aa911d96448d4f'
    Replit-Restored-To: bab961a887e155db6fad5e75f5aa911d96448d4f

--- darling-details-frontend/src/main.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-console.log("Main entry point loaded");
-
-createRoot(document.getElementById("root")!).render(<App />);
