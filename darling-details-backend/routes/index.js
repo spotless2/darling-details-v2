54ae946a1efd75a4a7b7080e548b1b0e60905735
Florin <florintudor.2k@gmail.com>
2025-04-23 04:40:18 +0300

    - added football matches api call (for sepparate project)

--- darling-details-backend/routes/index.js
+++ darling-details-backend/routes/index.js
@@ -7,6 +7,7 @@ const storeSettingsRoutes = require('./storeSettingsRoutes');
 const productRoutes = require('./productRoutes');
 const categoryRoutes = require('./categoryRoutes');
 const testimonialRoutes = require('./testimonialRoutes');
+const footballMatchesRoutes = require('./footballMatchesRoutes');
 
 // Use routes
 router.use('/users', userRoutes);
@@ -14,4 +15,5 @@ router.use('/store-settings', storeSettingsRoutes);
 router.use('/products', productRoutes);
 router.use('/categories', categoryRoutes);
 router.use('/testimonials', testimonialRoutes)
+router.use('/football-matches', footballMatchesRoutes);
 module.exports = router;
