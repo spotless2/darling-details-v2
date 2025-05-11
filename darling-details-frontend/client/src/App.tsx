c31d8f56d30f845a9817ca05415cf34974138d5f
Florin <florintudor.2k@gmail.com>
2025-05-12 02:46:44 +0300

    test - dark theme bug IOS chrome

--- darling-details-frontend/client/src/App.tsx
+++ darling-details-frontend/client/src/App.tsx
@@ -1,6 +1,5 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
-import { ThemeProvider } from "@/components/theme-provider";
 import { AppRoutes } from "@/routes";
 import { queryClient } from "@/lib/queryClient";
 import { MainLayout } from "@/components/layout/MainLayout";
@@ -8,12 +7,10 @@ import { MainLayout } from "@/components/layout/MainLayout";
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider defaultTheme="light" storageKey="theme-preference">
         <MainLayout>
           <AppRoutes />
         </MainLayout>
         <Toaster />
-      </ThemeProvider>
     </QueryClientProvider>
   );
 }
